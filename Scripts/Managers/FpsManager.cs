cc209762974bfe0f723c2e1965013682ad87ea75
Florian <florian.levreau@laposte.net>
2026-07-11 17:29:05 +0200

    Update FPSManager | Balance Enemies

--- Scripts/Managers/FpsManager.cs
+++ Scripts/Managers/FpsManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 /// Used to change fps max of the game , because by default the limit is 30fps
 /// </summary>
 public class FpsManager : MonoBehaviour {
+
+    private void Start() {
+        SetFps(60); // New
+    }
+
     public void SetFps(int fps) {
         QualitySettings.vSyncCount = 0;  // Disable VSync (ignores targetFrameRate if >0)
         Application.targetFrameRate = fps;  // Or -1 to uncap (not recommended for battery/heat)
